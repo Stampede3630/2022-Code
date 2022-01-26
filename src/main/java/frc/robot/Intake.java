d4d2c939050e592c7cfccd2d754ecce8efb82ec6
Dell 2019 DS Laptop <breckstampede@gmail.com>
2022-01-26 16:58:12 -0600

    Started Intake Code

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,22 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class Intake {
+
+
+    public static void spinIntake() {
+        TalonFX spinMotor = new TalonFX(0); //Filler port number 
+        //Actually make the motors spin on button press
+      if (Robot.xbox.getXButton() == true){
+        spinMotor.set(ControlMode.PercentOutput, 1);
+      }else{
+        spinMotor.set(ControlMode.PercentOutput, 0);  
+      }
+
+
+       
+    }
+    
+}
