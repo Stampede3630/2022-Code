acb07400467ca9c89f3f61a7a5ca0340f43378b5
Sam's Dell <vargoose84@gmail.com>
2022-01-28 12:47:39 -0600

    more commits... because vscode is funky

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Timer;
 import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
@@ -7,19 +8,27 @@ public class Climber implements Loggable{
     @Log
     boolean StateHasFinished;
     @Log
-    Boolean StateHasInitialized;
+    Boolean StateHasInitialized = false;
     @Log
     String CurrentState = "";
+
+    @Log
+    Timer climbTimer = new Timer();
     boolean StartingStateOverride;
 
     private static Climber SINGLE_INSTANCE = new Climber();
+    public static Climber getInstance() {
+        return SINGLE_INSTANCE;
+    }
     
     public static enum ClimberState{ 
         
-        STATE1(SINGLE_INSTANCE::myFirstAction, "STATE2"), 
-        STATE2(SINGLE_INSTANCE::myFirstAction, "STATE3"), 
-        STATE3(SINGLE_INSTANCE::myFirstAction, "STATE4"), 
-        STATE4(SINGLE_INSTANCE::DoneAction, "DONE");  
+        STATE1(SINGLE_INSTANCE::turnBlinker1On, "STATE2"), 
+        STATE2(SINGLE_INSTANCE::turnBlinker1On, "STATE3"), 
+        STATE3(SINGLE_INSTANCE::turnBlinker1On, "STATE4"), 
+        STATE4(SINGLE_INSTANCE::turnBlinker1On, "STATE5"), 
+        STATE5(SINGLE_INSTANCE::turnBlinker1On, "DONE"), 
+        DONE(SINGLE_INSTANCE::DoneAction, "DONE");  
 
         private Runnable action;
         private String nextState;
@@ -38,33 +47,6 @@ public class Climber implements Loggable{
         }
     }
 
-    public void myFirstAction(){
-        if(!StateHasInitialized){
-            //do things
-            
-            //These are required
-            StateHasFinished = false; 
-            StateHasInitialized = true;
-        }
-        
-        //do more things
-
-        //exit conditions
-        if(true) {
-            StateHasFinished  = true;
-        }
-    }
-
-    public void DoneAction(){
-        if(!StateHasInitialized){
-            //do things
-            
-            //These are required
-        }
-        //do more things
-        //NO EXIT CONDITIONS PLEASE
-    }
-
     public void climberRunner(String _startingState){
         if (_startingState != "" || StartingStateOverride){
             CurrentState = _startingState;
@@ -77,13 +59,119 @@ public class Climber implements Loggable{
 
         ClimberState.valueOf(CurrentState).getAction();
 
+        //if we made one round with the state, we have successfully initialized
+        if (!StateHasInitialized) {StateHasInitialized = true;}
+
         if (StateHasFinished){
             CurrentState = ClimberState.valueOf(CurrentState).getNextState();
             StateHasFinished = false; 
-            StateHasInitialized = true;
+            StateHasInitialized = false;
         }
     }
 
+    @Log
+    Boolean blinker1 = false;
+    @Log
+    Boolean blinker2 = false;
+    @Log
+    Boolean blinker3 = false;
+    @Log
+    Boolean blinker4 = false;
+    @Log
+    Boolean blinker5 = false;
+
+    public void turnBlinker1On(){
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        blinker1 = true;
+        if(climbTimer.hasElapsed(5)) {
+            climbTimer.stop();
+            climbTimer.reset();
+            StateHasFinished  = true;
+        }
+    }
+
+    public void turnBlinker2On(){
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        blinker2 = true;
+        if(climbTimer.hasElapsed(5)) {
+            climbTimer.stop();
+            climbTimer.reset();
+            StateHasFinished  = true;
+        }
+    }
+
+    public void turnBlinker3On(){
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        blinker3= true;
+        if(climbTimer.hasElapsed(5)) {
+            climbTimer.stop();
+            climbTimer.reset();
+            StateHasFinished  = true;
+        }
+    }
+
+    
+    public void turnBlinker4On(){
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        blinker4 = true;
+        if(climbTimer.hasElapsed(5)) {
+            climbTimer.stop();
+            climbTimer.reset();
+            StateHasFinished  = true;
+        }
+    }
+
+    public void turnBlinker5On(){
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        blinker5 = true;
+        if(climbTimer.hasElapsed(5)) {
+            climbTimer.stop();
+            climbTimer.reset();
+            StateHasFinished  = true;
+        }
+    }
+
+
+    /* public void myFirstAction(){
+        if(!StateHasInitialized){
+
+        }
+        
+        //do more things
+
+        //exit conditions
+        if(true) {
+            StateHasFinished  = true;
+        }
+    }*/
+
+    public void DoneAction() {
+        if(!StateHasInitialized){
+            climbTimer.start();
+        }
+        if (climbTimer.hasElapsed(5)) {
+            blinker1 = !blinker1;
+            blinker2 = !blinker2;
+            blinker3 = !blinker3;
+            blinker4 = !blinker4;
+            blinker5 = !blinker5;
+            climbTimer.stop();
+            climbTimer.reset();
+            climbTimer.start();
+        }
+        
+
+    } 
 
 
     
