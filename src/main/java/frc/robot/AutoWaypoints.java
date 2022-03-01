17210aeba8fe34e9ec2e235a534f06241c777dcb
Sam <vargoose84@gmail.com>
2022-03-01 00:25:49 -0600

    minor changes to fix runtime issues

--- src/main/java/frc/robot/AutoWaypoints.java
+++ src/main/java/frc/robot/AutoWaypoints.java
@@ -8,7 +8,8 @@ import io.github.oblarg.oblog.annotations.Log;
 public class AutoWaypoints implements Loggable {
 
     private static AutoWaypoints SINGLE_INSTANCE = new AutoWaypoints();
-
+    public Waypoint[] FenderTwoBallAutoWPs;
+    public Waypoint[] FenderFourBallAutoWPs; 
     public PathPlannerTrajectory fourBallAutoPath;
     public PathPlannerTrajectory twoBallAutoPath;
     public Waypoint[] chosenWaypoints;
@@ -16,6 +17,16 @@ public class AutoWaypoints implements Loggable {
     private double currentX;
     private double currentY;
     public AutoPoses chosenPath;
+
+    @Log
+    public boolean StateHasFinished = false;
+    @Log
+    public Boolean StateHasInitialized = false;
+    @Log(tabName = "CompetitionLogger")
+    public double distance = 0;
+    
+    @Log(tabName = "CompetitionLogger")
+    public SendableChooser<AutoPoses> m_autoChooser = new SendableChooser<>();
  
     public static AutoWaypoints getInstance() {
         return SINGLE_INSTANCE;
@@ -30,6 +41,20 @@ public class AutoWaypoints implements Loggable {
     }
 
     public void loadAutoPaths(){
+       FenderTwoBallAutoWPs = new Waypoint[] {
+            new Waypoint(SINGLE_INSTANCE::intakeBall, 7.65, .060),
+            new Waypoint(SINGLE_INSTANCE::shoot, 7.88, 2.86),
+            new Waypoint(SINGLE_INSTANCE::done, 0, 0) 
+        };
+    
+        FenderFourBallAutoWPs = new Waypoint[] {
+            new Waypoint(SINGLE_INSTANCE::intakeBall, 7.801, 1.678),
+            new Waypoint(SINGLE_INSTANCE::shoot, 7.882, 2.952),
+            new Waypoint(SINGLE_INSTANCE::intakeBall, 5.278, 2.014),
+            new Waypoint(SINGLE_INSTANCE::intakeBall, 1.273, 1.215),
+            new Waypoint(SINGLE_INSTANCE::shoot, 7.581, 3.033),
+            new Waypoint(SINGLE_INSTANCE::done, 0, 0) 
+        };
         chooserBuilder();
         fourBallAutoPath = PathPlanner.loadPath("blueAutoTest", 3, 2.5);
         twoBallAutoPath = PathPlanner.loadPath("twoBallAuto", 3, 2.5);
@@ -40,14 +65,6 @@ public class AutoWaypoints implements Loggable {
         currentY = Robot.SWERVEDRIVE.getYPos();
         waypointRunner(chosenWaypoints);
     }
-
-    @Log
-    boolean StateHasFinished = false;
-    @Log
-    Boolean StateHasInitialized = false;
-    
-    @Log(tabName = "CompetitionLogger")
-    public SendableChooser<AutoPoses> m_autoChooser = new SendableChooser<>();
 
     public enum AutoPoses {
         FENDERFOURBALLAUTO(7.80, 1.68, -84.69, SINGLE_INSTANCE.FenderFourBallAutoWPs,SINGLE_INSTANCE.fourBallAutoPath),
@@ -68,21 +85,6 @@ public class AutoWaypoints implements Loggable {
 
         }
     }
-
-    public Waypoint[] FenderTwoBallAutoWPs = new Waypoint[] {
-        new Waypoint(SINGLE_INSTANCE::intakeBall, 7.65, .060),
-        new Waypoint(SINGLE_INSTANCE::shoot, 7.88, 2.86),
-        new Waypoint(SINGLE_INSTANCE::done, 0, 0) 
-    };
-
-    public Waypoint[] FenderFourBallAutoWPs = new Waypoint[] {
-        new Waypoint(SINGLE_INSTANCE::intakeBall, 7.801, 1.678),
-        new Waypoint(SINGLE_INSTANCE::shoot, 7.882, 2.952),
-        new Waypoint(SINGLE_INSTANCE::intakeBall, 5.278, 2.014),
-        new Waypoint(SINGLE_INSTANCE::intakeBall, 1.273, 1.215),
-        new Waypoint(SINGLE_INSTANCE::shoot, 7.581, 3.033),
-        new Waypoint(SINGLE_INSTANCE::done, 0, 0) 
-    };
 
     private void intakeBall() {
         //SJV: NEED TO TURN INTAKE OFF WHEN STATE IS FINISHED, NEED TO PROLLY LOWER THE DISTANCE FROM HALF A METER TO LESS (TEST PLEASE)
@@ -121,8 +123,8 @@ public class AutoWaypoints implements Loggable {
        }
     }
 
-    private double getDistance(double X1, double Y1, double X2, double Y2) { //just the distance formula - uses current x and y positions
-        double distance = Math.sqrt(Math.pow((X2 - X1), 2) + Math.pow((Y2 - Y1), 2));
+    public double getDistance(double X1, double Y1, double X2, double Y2) { //just the distance formula - uses current x and y positions
+        distance = Math.sqrt(Math.pow((X2 - X1), 2) + Math.pow((Y2 - Y1), 2));
         return distance;
     }
 
