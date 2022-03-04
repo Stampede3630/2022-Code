4b90163425bde5c128464fdcacdbb4a6c3927028
warfalcon64 <peppythepig8@gmail.com>
2022-03-04 10:10:28 -0600

    added stuff for hub tracking in limelight

--- src/main/java/frc/robot/Limelight.java
+++ src/main/java/frc/robot/Limelight.java
@@ -4,13 +4,22 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Limelight { // There are currently no comments explaining how this code works :(
+
+    private static Limelight SINGLE_INSTANCE = new Limelight();
+
     private static NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
     private static double camAngle; // CRITICAL: get camAngle from servo for limelight
     private static final double camHeight = 0; // CRITICAL: measure height of limelight from the ground
 
+    public static boolean isLimelightOpen = false;
+
+    public static Limelight getInstance() {
+        return SINGLE_INSTANCE;
+    }
+
     // methods for vision processing and LED aren't currently implemented
     public void limelightPeriodic() {
-
+        // Put methods you want in here
     }
 
     public static class Hub {
@@ -29,7 +38,7 @@ public class Limelight { // There are currently no comments explaining how this 
 
         private static TargetType trackedTargetType = TargetType.UNDEFINED;
 
-        public void reset() {
+        public static void reset() {
             xDistance = 0;
             yDistance = 0;
             xVelocity = 0;
@@ -39,9 +48,46 @@ public class Limelight { // There are currently no comments explaining how this 
             trackedTargetType = TargetType.UNDEFINED;
         }
 
-        // unfinished
-        public static void getPosition(double camAngle, double camHeight) {
+        public static boolean trackHub(TargetType targetType) {
+            if (trackedTargetType != targetType) {
+                reset();
+            }
+
+            switch (targetType) {
+                case UNDEFINED:
+                    break;
+            
+                case UPPERGOAL:
+                    getPosition(camAngle, camHeight);
+                    break;
+            }
+
+            return isValid();
+        }
+
+        private static void getPosition(double camAngle, double camHeight) {
             double yAngle = 90 + camAngle + Limelight.getTY();
+
+            if (!Limelight.isTargetValid() || (camHeight < 0 && yAngle > 89.99) || (camHeight > 0 && yAngle < 89.99)) {
+                valid = false;
+                return;
+            }
+
+            double newX = Math.tan(Math.toRadians(Limelight.getTX())) * yDistance + camHeight;
+            double newY = Math.tan(Math.toRadians(yAngle)) * -camHeight;
+
+            if (valid) {
+                xVelocity = newX - xDistance;
+                yVelocity = newY - yDistance;
+            } else {
+                xVelocity = 0;
+                yVelocity = 0;
+            }
+
+            valid = true;
+            xDistance = newX;
+            yDistance = newY;
+            angle = Math.atan2(xDistance, yDistance) / Math.PI * 180;
         }
 
         // --- Hub Getters ---
@@ -93,7 +139,7 @@ public class Limelight { // There are currently no comments explaining how this 
         limelight.getEntry(entry).setDouble(value);
     }
     
-    public static boolean getTV() {
+    public static boolean isTargetValid() {
         return getEntry("tv") == 1;
     }
 
