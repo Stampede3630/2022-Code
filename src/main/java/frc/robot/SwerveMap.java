8f7d28a4df26d16728fe1955b29023c0540ab451
Dell 2019 DS Laptop <breckstampede@gmail.com>
2022-03-10 15:58:03 -0600

    added canivore crap to swerve

--- src/main/java/frc/robot/SwerveMap.java
+++ src/main/java/frc/robot/SwerveMap.java
@@ -77,7 +77,7 @@ public class SwerveMap {
         public TalonFXInvertType kWheelDirectionType;
         public Constants.Gains kGAINS;
         public DriveMotor (int _talonID, TalonFXInvertType _direction, Constants.Gains _gains){
-            super(_talonID);
+            super(_talonID, "swerve CANIVORE");
             kWheelDirectionType = _direction;
             kGAINS=_gains;
         }
@@ -87,7 +87,7 @@ public class SwerveMap {
         public double kOffsetDegrees;
 
         public SteeringSensor (int _sensorID, double _offsetDegrees){
-            super(_sensorID);
+            super(_sensorID, "swerve CANIVORE");
             kOffsetDegrees = _offsetDegrees;
          }       
     }
